37c3ee20841023a1310da7d076086bb2eadfd498
fayezabdulrahman <c15321396@mydit.ie>
2020-05-17 20:23:44 +0100

    added dummy authentication process for now

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import { NavController, LoadingController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './service/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -9,6 +10,7 @@ import { AuthService } from './service/auth.service';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService, private navController: NavController, private loadingController: LoadingController) { }
 
@@ -27,5 +29,24 @@ export class AuthPage implements OnInit {
           this.navController.navigateForward('/places/tabs/discover');
         }, 1000);
       });
+  }
+
+  submitLogin(formLogin: NgForm) {
+    if(!formLogin.valid) {
+      return;
+    }
+
+    const email = formLogin.value.email;
+    const password = formLogin.value.password;
+    console.log(email, password);
+    if(this.isLogin) {
+      // send a request to login server
+    } else {
+      // send a request to sign up server
+    }
+  }
+
+  switchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
